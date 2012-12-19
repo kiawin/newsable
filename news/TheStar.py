6d92cc762707f07ec1cecd1e34bd6eb191d4db0b
kiawin <kiawin@gmail.com>
2012-12-20 01:12:08 +0800

    [#40680193] Refactoring

--- news/TheStar.py
+++ news/TheStar.py
@@ -26,7 +26,7 @@ class TheStar():
                                 'url': 'http://thestar.com.my/news/nation/',
                                 'tags': ['nation'],
                                 'language': self.default_language,
-                                'expr': self.default_expression,
+                                'news_source_expr': self.default_expression,
                                 'news_item_expr': self.default_news_item_expression,
                                 'url_prefix': self.default_url_prefix
                                 },
@@ -34,77 +34,77 @@ class TheStar():
                                  'url': 'http://thestar.com.my/news/sarawak/',
                                  'tags': ['nation','sarawak'],
                                  'language': self.default_language,
-                                 'expr': self.default_expression,
+                                 'news_source_expr': self.default_expression,
                                  'url_prefix': self.default_url_prefix
                                  },
                      'business': {
                                   'url': 'http://biz.thestar.com.my/news/',
                                   'tags': ['business'],
                                   'language': self.default_language,
-                                  'expr': self.default_expression,
+                                  'news_source_expr': self.default_expression,
                                   'url_prefix': self.default_url_prefix
                                   },
                      'metro-central': {
                                        'url': 'http://thestar.com.my/metro/central/',
                                        'tags': ['metro','central'],
                                        'language': self.default_language,
-                                       'expr': self.default_expression,
+                                       'news_source_expr': self.default_expression,
                                        'url_prefix': self.default_url_prefix
                                        },
                      'metro-north': {
                                      'url': 'http://thestar.com.my/metro/north/',
                                      'tags': ['metro','north'],
                                      'language': self.default_language,
-                                     'expr': self.default_expression,
+                                     'news_source_expr': self.default_expression,
                                      'url_prefix': self.default_url_prefix
                                      },
                      'metro-biz': {
                                    'url': 'http://thestar.com.my/metro/biz/',
                                    'tags': ['metro','business'],
                                    'language': self.default_language,
-                                   'expr': self.default_expression,
+                                   'news_source_expr': self.default_expression,
                                    'url_prefix': self.default_url_prefix
                                    },
                      'metro-southneast': {
                                           'url': 'http://thestar.com.my/metro/southneast/',
                                           'tags': ['metro','south','east'],
                                           'language': self.default_language,
-                                          'expr': self.default_expression,
+                                          'news_source_expr': self.default_expression,
                                           'url_prefix': self.default_url_prefix
                                           },
                      'metro-perak': {
                                      'url': 'http://thestar.com.my/metro/perak/',
                                      'tags': ['metro','perak','north'],
                                      'language': self.default_language,
-                                     'expr': self.default_expression,
+                                     'news_source_expr': self.default_expression,
                                      'url_prefix': self.default_url_prefix
                                      },
                      'courts': {
                                 'url': 'http://thestar.com.my/news/courts/',
                                 'tags': ['courts'],
                                 'language': self.default_language,
-                                'expr': self.default_expression,
+                                'news_source_expr': self.default_expression,
                                 'url_prefix': self.default_url_prefix
                                 },
                      'parliament': {
                                     'url': 'http://thestar.com.my/news/parliament/',
                                     'tags': ['parliament'],
                                     'language': self.default_language,
-                                    'expr': self.default_expression,
+                                    'news_source_expr': self.default_expression,
                                     'url_prefix': self.default_url_prefix
                                     },
                      'opinion': {
                                  'url': 'http://thestar.com.my/news/opinion/',
                                  'tags': ['opinion'],
                                  'language': self.default_language,
-                                 'expr': self.default_expression,
+                                 'news_source_expr': self.default_expression,
                                  'url_prefix': self.default_url_prefix
                                  },
                      'maritime': {
                                   'url': 'http://thestar.com.my/maritime/',
                                   'tags': ['maritime','business'],
                                   'language': self.default_language,
-                                  'expr': self.default_expression,
+                                  'news_source_expr': self.default_expression,
                                   'url_prefix': self.default_url_prefix
                                   }
                      }
@@ -157,7 +157,7 @@ class TheStar():
         scraper = Scraper(details['url'])
         scrap = scraper.get()
         self._logger.debug("Section: "+source)
-        for news in scrap.select(details['expr']):
+        for news in scrap.select(details['news_source_expr']):
             #title = str(news.contents)
             url = details['url_prefix'] + news['href']
             title = self.sanitize(news.contents[0])
