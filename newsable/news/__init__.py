d7207a1422155d08a913f11dfa8595cc3f32e569
kiawin <kiawin@gmail.com>
2012-12-20 01:54:37 +0800

    [#40680193] Refactoring

---
+++ newsable/news/__init__.py
@@ -0,0 +1,8 @@
+from newsable.Logger import Logger
+from newsable.Scraper import Scraper
+from newsable.DB import MongoDB
+from newsable.Stripper import Stripper
+from newsable.NewsSource import NewsSource
+from newsable.NewsItem import NewsItem
+
+from newsable.news import TheStar
