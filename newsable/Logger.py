b55c187283a5a32bf57ff37a83219cfd8c149328
kiawin <kiawin@gmail.com>
2013-02-25 19:35:42 +0800

    [#40680193] Add log directory if not exist

--- newsable/Logger.py
+++ newsable/Logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 
 class Logger():
@@ -6,10 +7,17 @@ class Logger():
     def __init__(self, news):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
-        self._fileHandler = logging.FileHandler('../logs/'+news+'.log')
+        self._fileDirectoryPath = './'
+        self._fileDirectory = 'logs'
+        self.checkDirectoryExist(self._fileDirectoryPath+self._fileDirectory)
+        self._fileHandler = logging.FileHandler(self._fileDirectoryPath+self._fileDirectory+'/'+news+'.log')
         self._formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         self._fileHandler.setFormatter(self._formatter)
         self.logger.addHandler(self._fileHandler)
+        
+    def checkDirectoryExist(self, directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
         
     def get(self):
         return self.logger
